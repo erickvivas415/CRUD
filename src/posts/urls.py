2eba35b74bd5f6c31a64eef32b5c734ed2677763
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-10-12 15:44:00 -0700

    erro with ajax

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,6 +3,7 @@ from .views import (
     hello_world_view,
     load_post_data_view,
     post_list_and_create,
+    like_unlike_post,
 )
 
 app_name = 'posts'
@@ -10,5 +11,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name = 'main-board'),
     path('data/<int:numPosts>/', load_post_data_view, name = 'posts-data'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('hello-world/', hello_world_view, name = "hello-world"),
 ]
